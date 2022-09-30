5b12d5e161dda2eaf53d169a3d41aed698e5a560
ndinkwa <105945681+ndinkwa@users.noreply.github.com>
2022-09-30 16:05:45 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
     bucket = "vanny234"
-    key    = "avinash/s3file-3.tfstate"
+    key    = "test-tfstatefile"
     region = "us-east-1"
     profile = "jespo"
 dynamodb_table= "project4-statefile"

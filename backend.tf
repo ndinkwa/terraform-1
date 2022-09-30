0a8b0bcb03f337b4b79571846bb242e0f3b9d597
ndinkwa <105945681+ndinkwa@users.noreply.github.com>
2022-09-30 14:04:53 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
     bucket = "vanny234"
     key    = "avinash/s3file-3.tfstate"
     region = "us-east-1"
-    profile = "jbbrown"
+    profile = "jespo"
 dynamodb_table= "project4-statefile"
    }
  }
